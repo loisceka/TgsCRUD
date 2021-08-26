cba7d411708c240a0bd479906e2ac00a940577f2
loisceka <loisceka13@gmail.com>
2021-08-26 19:32:09 +0700

    initial model
    adding depaartment, job, employee model

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -5,10 +5,135 @@
  */
 package models;
 
+import java.sql.Date;
+
 /**
  *
  * @author loisceka
  */
 public class Employee {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Date birthDate;
+    private int salary;
+    private double commision;
+    private String jobId;
+    private String managerId;
+    private String departmentId;
+
+    public Employee() {
+    }
+
+    public Employee(String id, String firstName, String lastName, String email, String phone, Date birthDate, int salary, double commision, String jobId, String managerId, String departmentId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.salary = salary;
+        this.commision = commision;
+        this.jobId = jobId;
+        this.managerId = managerId;
+        this.departmentId = departmentId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public double getCommision() {
+        return commision;
+    }
+
+    public void setCommision(double commision) {
+        this.commision = commision;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(String managerId) {
+        this.managerId = managerId;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", phone=" + phone + ", birthDate=" + birthDate + ", salary=" + salary + ", commision=" + commision + ", jobId=" + jobId + ", managerId=" + managerId + ", departmentId=" + departmentId + '}';
+    }
+
     
 }
--- src/models/Job.java
+++ src/models/Job.java
@@ -10,5 +10,57 @@ package models;
  * @author loisceka
  */
 public class Job {
-    
+
+    private String id;
+    private String name;
+    private int minSalary;
+    private int maxSalary;
+
+    public Job() {
+    }
+
+    public Job(String id, String name, int minSalary, int maxSalary) {
+        this.id = id;
+        this.name = name;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(int minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public int getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(int maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" + "id=" + id + ", name=" + name + ", minSalary=" + minSalary + ", maxSalary=" + maxSalary + '}';
+    }
+
 }
