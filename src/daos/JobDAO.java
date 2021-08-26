1e5e130ff2ad42769c8ed5bf07900d897b2a3004
loisceka <loisceka13@gmail.com>
2021-08-26 20:10:19 +0700

    add Department CRUD
    add department crud (manager id and location id cannot be null)

--- src/daos/JobDAO.java
+++ src/daos/JobDAO.java
@@ -12,7 +12,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import models.Job;
-import models.Region;
 
 /**
  *
--- src/tools/TgsCRUD.java
+++ src/tools/TgsCRUD.java
@@ -9,6 +9,7 @@ import daos.DepartmentDAO;
 import daos.EmployeeDAO;
 import daos.JobDAO;
 import daos.RegionDAO;
+import models.Department;
 import models.Job;
 import models.Region;
 
@@ -29,16 +30,16 @@ public class TgsCRUD {
         //Dependency Injection
         RegionDAO rdao = new RegionDAO(dbc.getConnection());
         JobDAO jdao = new JobDAO(dbc.getConnection());
-        EmployeeDAO emdao = new EmployeeDAO(dbc.getConnection());
-        DepartmentDAO dedao = new DepartmentDAO(dbc.getConnection());
+        EmployeeDAO empdao = new EmployeeDAO(dbc.getConnection());
+        DepartmentDAO depdao = new DepartmentDAO(dbc.getConnection());
         
-//          System.out.println(jdao.insert(new Job("A2000", "Tester", 400, 1000)));
-//        System.out.println(jdao.update("A2000", new Job("A2100", "Consultant", 600, 1500)));
-//        System.out.println(jdao.delete("A2100"));
-//        System.out.println(jdao.getById("A1000"));
+//        System.out.println(depdao.insert(new Department("500B", "Consulant", "ID01", "1005")));
+//        System.out.println(depdao.update("500B", new Department("50BC", "Throw", "ID02", "1006")));
+//        System.out.println(depdao.delete("50BC"));
+//        System.out.println(depdao.getById("100"));
         
-        for (Job j : jdao.getAll()) {
-            System.out.println(j.getId()+" - "+j.getName());
+        for (Department dep : depdao.getAll()) {
+            System.out.println(dep.getId()+" - "+dep.getName());
         }
     }
 }
